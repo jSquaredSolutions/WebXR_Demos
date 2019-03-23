39602e4537227b10b0ca5cd871252ad6b04a037a
Jsquared <info@jsquaredsolutions.net>
2019-03-23 19:43:55 -0400

    PulsingSphere

--- vive.js
+++ vive.js
@@ -1,9 +1,23 @@
-setInterval(function(){ 
+/* setInterval(function(){ 
     gameP = navigator.getGamepads();
     gp = gameP[0];
     console.log("x: "+gp.pose.position[0]);
     console.log("y: "+gp.pose.position[1]);
     console.log("z: "+gp.pose.position[2]);
-  }, 500);
+  }, 100); */
 
   /* I originally at this JS in the HTML file, but I could not set breakpoints there */
+  
+  /*
+  setTimeout(function(){
+    console.log("helloWorld");
+    el = document.querySelector("a-sphere");
+    elY = 0;
+    setInterval(function(){ 
+      el.object3D.position.set(0, elY+.01, 0);
+      console.log("helloWorld2");
+    }, 1000); 
+  }, 3000);
+   */
+
+ 
