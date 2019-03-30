e12bf6c781e6814f7ea15c33e84ad8f858d2731a
Jsquared <info@jsquaredsolutions.net>
2019-03-30 16:47:49 -0400

    xxx

--- demo.js
+++ demo.js
@@ -40,6 +40,21 @@ var server = http.createServer(function(request, response) {
                     response.end();  
                 }  
             });  
+            break;   
+            case '/x2.mp3':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
             break;    
         default:  
             response.writeHead(404);  
